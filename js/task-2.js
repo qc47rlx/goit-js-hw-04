a47ca2ac8c64786d92475caa001a7a08a5f9af0f
Anton Postnov <139925457+qc47rlx@users.noreply.github.com>
2024-03-14 12:11:59 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,14 @@
 function calcAverageCalories(days) {
   let totalCalories = 0;
-
+  if (days.length === 0) {
+    return 0;
+  }
   for (const day of days) {
     totalCalories += day.calories;
   }
   return totalCalories / days.length;
 }
+
 
 console.log(
   calcAverageCalories([
